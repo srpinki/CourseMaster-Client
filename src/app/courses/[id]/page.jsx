adff0e7b4d297c301a8b622f8f0abd4d308eff4f
srpinki <pinkibiswas0295@gmail.com>
2025-12-05 09:57:53 +0600

    fixed checkout issue

--- src/app/courses/[id]/page.jsx
+++ src/app/courses/[id]/page.jsx
@@ -35,7 +35,7 @@ const handleEnroll = async () => {
   }
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/checkout/create-session`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/checkout/create-session`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
